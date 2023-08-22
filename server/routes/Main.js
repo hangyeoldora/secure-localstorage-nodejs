6e5e86dc98429540a56edc76b82c3ce076364da7
hangyeoldora <hg14699@naver.com>
2023-08-23 00:40:56 +0900

    [UPDATE] add decrypted hash string and save to mysql

--- server/routes/Main.js
+++ server/routes/Main.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const macAddress = require("macaddress");
 const CryptoJS = require("crypto-js");
 const crypto = require("crypto");
-const jsencrypt = require('nodejs-jsencrypt');
+const jsencrypt = require("nodejs-jsencrypt");
+const { encryptedId } = require("../models");
+
 require("dotenv").config();
 
 const secretKey = "YrvfhEoPhjAGC28R1q78zkD2ydrEcIxs";
@@ -16,7 +18,7 @@ const aesIv = {
 
 // 고유식별정보 - 맥 주소
 router.get("/getMac", (req, res) => {
-  macAddress.one().then(mac => {
+  macAddress.one().then((mac) => {
     // aes 암호화 처리
     const cipher = CryptoJS.AES.encrypt(
       mac,
@@ -40,7 +42,7 @@ router.get("/getMac", (req, res) => {
           mode: CryptoJS.mode.CBC,
         }
       );
-  
+
       decodeStr = cipher.toString(CryptoJS.enc.Utf8);
       console.log("data : " + cipher);
     }
@@ -55,18 +57,31 @@ router.get("/getPk", (req, res) => {
 
 // 암호문 저장
 router.post("/setDb", async (req, res) => {
-  const encStr = await req.body;
-  console.log(encStr.enc);
+  const { enc } = await req.body;
+  console.log(enc);
   // const sKey = CryptoJS.lib.WordArray.random(128 / 8); // salt 16
   const decrypt = new jsencrypt.JSEncrypt();
   decrypt.setPrivateKey(process.env.PRIV_PEM);
-  const uncrypted = decrypt.decrypt(encStr.enc);
+  const uncrypted = decrypt.decrypt(enc);
   console.log(uncrypted);
+  const newSecretKey = "OvLIdkJyTpSkDMa1hPcJ93OCP5Qkvtlk";
 
-  const newSecretKey = 'OvLIdkJyTpSkDMa1hPcJ93OCP5Qkvtlk';
-  
-  res.send({ nice: encStr.enc });
- 
+  try {
+    await encryptedId.create({
+      hash: uncrypted.toString(),
+      key: newSecretKey,
+    });
+  } catch {}
+
+  // hash 중복 방지 추가로 해당 조건 추가 필요
+  const id = await encryptedId.findOne({
+    where: { hash: uncrypted.toString() },
+  });
+  if (!id) {
+    res.json({ error: "hash 에러 발생" });
+  } else {
+    res.send({ id: id.id });
+  }
 });
 
 module.exports = router;
